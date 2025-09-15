96d14196c2908043839cef0ab46a09b50dedd2d2
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-09-14 22:46:22 -0400

    update APP_ID

--- frontend/src/lib/linera/constants.ts
+++ frontend/src/lib/linera/constants.ts
@@ -2,7 +2,7 @@
 export const LINERA_RPC_URL = "https://faucet.testnet-conway.linera.net/";
 
 // Game of Life application ID
-export const GOL_APP_ID = "cc918f81c841b28498ca0c6c3c1c131b9d8f1257c40639de984aac3212edaab8";
+export const GOL_APP_ID = "c5b78269c7dfe9d121eab606c8f3ecd240de64d25b533b6f1009ac51c03d78ab";
 
 // Dynamic wallet configuration
 export const DYNAMIC_ENVIRONMENT_ID = "18db11c3-bdb1-44dc-a933-ba80ff8635c1";
