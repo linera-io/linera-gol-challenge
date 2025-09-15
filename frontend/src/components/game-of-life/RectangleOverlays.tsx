c4aeeed1bbffa312fe489df1989db558c35116c2
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-09-14 23:50:52 -0400

    fix pnpm build

--- frontend/src/components/game-of-life/RectangleOverlays.tsx
+++ frontend/src/components/game-of-life/RectangleOverlays.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { Condition } from "@/lib/types/puzzle.types";
 import { RECTANGLE_COLORS, getRGBString } from "@/lib/game-of-life/config/colors";
 import {
